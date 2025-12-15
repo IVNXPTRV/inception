1862854f797ed88a0a14d4c1b6faabee39b1f2b2
Ivan Petrov <ivnxptrv@frutro.localdomain>
2025-12-15 04:44:18 -0700

    feat: add half working config

--- srcs/requirements/mariadb/tools/configure.sh
+++ srcs/requirements/mariadb/tools/configure.sh
@@ -1,48 +1,43 @@
-#!/usr/bin/env sh
+#!/bin/env sh
 
 set -e
 
-mkdir -p /run/mysqld
-chown mysql:mysql /run/mysqld
-
-if [ -f ./wp-config.php ]; then
-	echo "wordpress already configured"
-    exec "$@"
+if [ ! -d "/run/mysqld" ]; then
+	mkdir -p /run/mysqld
+	chown -R mysql:mysql /run/mysqld
 fi
-    
-DB_PASSWORD=$(cat /run/secrets/db_password.txt)
-WP_ADMIN_PASSWORD=$(cat /run/secrets/wp_admin_password.txt)
-WP_USER_PASSWORD=$(cat /run/secrets/wp_user_password.txt)
 
-# connect to db
-# sed -i "s/database_name_here/$DB_NAME/" wp-config.php 
-# sed -i "s/username_here/$DB_USER/" wp-config.php 
-# sed -i "s/password_here/$DB_PASSWORD/" wp-config.php 
-# sed -i "s/localhost/$DB_HOST/" wp-config.php
-wp config create --allow-root \
-        --dbname="$DB_NAME" \
-        --dbuser="$DB_USER" \
-        --dbhost="$DB_HOST" \
-        --dbpass="$DB_PASSWORD" \
-        --dbcharset="utf8" \
-        --dbcollate="utf8_general_ci" \
-        --path='/var/www/html'
+# rm -rf /var/lib/mysql/
 
-# install wordpress core
-wp core install \
-        --url="$WP_SITE_URL" \
-        --title="$WP_TITLE" \
-        --admin_user="$WP_ADMIN_NAME" \
-        --admin_email="$WP_ADMIN_EMAIL" \
-        --admin_password="$WP_ADMIN_PASSWORD" \
-        --allow-root
+if [ ! -d "/var/lib/mysql/mysql" ] ||  [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
 
-# create additional user
-wp user create \
-        "$WP_USER_NAME" \
-        "$WP_USER_EMAIL" \
-        --user_pass="$WP_USER_PASSWORD" \
-        --role=author \
-        --allow-root
+    # Install the database
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 
+    # Start temporary instance
+    mysqld --user=mysql --skip-networking &
+    MYSQL_PID=$!
+
+    # Wait a bit for server to start
+    sleep 10
+
+    DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
+    DB_USER_PASSWORD=$(cat /run/secrets/db_user_password)
+
+    # Setup database and users
+    mysql << EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DROP DATABASE IF EXISTS test;
+CREATE DATABASE $DB_NAME;
+CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASSWORD';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+    # Stop temporary instance
+    mysqladmin -u root -p${DB_ROOT_PASSWORD} shutdown
+    wait $MYSQL_PID
+fi
 exec "$@"
--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -1,45 +1,79 @@
 #!/usr/bin/env sh
 
+# wp core download --allow-root --path="/var/www/html"
+# wp core is-installed --allow-root --path="/var/www/html" 
+# wp config create --allow-root \
+#     --dbname="wordpress" \
+#     --dbuser="wp_user" \
+#     --dbhost="172.17.0.3:3306" \
+#     --dbpass="bonamp-tofasi" \
+#     --dbcharset="utf8" \
+#     --dbcollate="utf8_general_ci" \
+#     --path="/var/www/html"
+    
+# wp core install --allow-root \
+#         --url="ipetrov.42.fr" \
+#         --title="Inception" \
+#         --admin_user="muzzle" \
+#         --admin_email="muzzle@m.c" \
+#         --admin_password="1234" \
+#         --path="/var/www/html/"
+
+# wp user create --allow-root \
+#         "$WP_USER_NAME" \
+#         "$WP_USER_EMAIL" \
+#         --user_pass="$WP_USER_PASSWORD" \
+#         --role=author \
+#         --path="$WP_PATH"
+
 set -e
 
-if [ -f ./wp-config.php ]; then
-	echo "wordpress already configured"
+WP_PATH="/var/www/html/"
+
+# Download WordPress if the directory is empty
+echo "Downloading WordPress if required..."
+wp core download --allow-root --path="$WP_PATH"
+
+if wp core is-installed --allow-root --path="$WP_PATH"; then
+    echo "WordPress is already installed and ready."
     exec "$@"
 fi
-    
-DB_PASSWORD=$(cat /run/secrets/db_password.txt)
-WP_ADMIN_PASSWORD=$(cat /run/secrets/wp_admin_password.txt)
-WP_USER_PASSWORD=$(cat /run/secrets/wp_user_password.txt)
 
-# connect to db
-# sed -i "s/database_name_here/$DB_NAME/" wp-config.php 
-# sed -i "s/username_here/$DB_USER/" wp-config.php 
-# sed -i "s/password_here/$DB_PASSWORD/" wp-config.php 
-# sed -i "s/localhost/$DB_HOST/" wp-config.php
+echo "Waiting for database connection..."
+    while ! nc -z "$DB_HOST" "$DB_PORT"; do
+        sleep 1
+    done
+echo "Database is ready! Starting WordPress configuration."
+
+DB_USER_PASSWORD=$(cat /run/secrets/db_user_password)
+
 wp config create --allow-root \
         --dbname="$DB_NAME" \
         --dbuser="$DB_USER" \
-        --dbhost="$DB_HOST" \
-        --dbpass="$DB_PASSWORD" \
+        --dbhost="$DB_HOST:$DB_PORT" \
+        --dbpass="$DB_USER_PASSWORD" \
         --dbcharset="utf8" \
         --dbcollate="utf8_general_ci" \
-        --path='/var/www/html'
+        --path="$WP_PATH"
 
-# install wordpress core
-wp core install \
+# Install WordPress if it's not installed
+echo "Installing WordPress..."
+WP_ADMIN_PASSWORD=$(cat /run/secrets/wp_admin_password)
+WP_USER_PASSWORD=$(cat /run/secrets/wp_user_password)
+
+wp core install --allow-root \
         --url="$WP_SITE_URL" \
         --title="$WP_TITLE" \
         --admin_user="$WP_ADMIN_NAME" \
         --admin_email="$WP_ADMIN_EMAIL" \
         --admin_password="$WP_ADMIN_PASSWORD" \
-        --allow-root
+        --path="$WP_PATH"
 
-# create additional user
-wp user create \
+wp user create --allow-root \
         "$WP_USER_NAME" \
         "$WP_USER_EMAIL" \
         --user_pass="$WP_USER_PASSWORD" \
         --role=author \
-        --allow-root
+        --path="$WP_PATH"
 
 exec "$@"
